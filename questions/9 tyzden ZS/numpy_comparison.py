f6ab7352a4c343a15a9768db20a9a160f516e511
Vladyslav Tsimbota <vladyslav.tsimbota@gymbeam.com>
2025-11-12 09:35:15 +0100

    feat: add clarity regarding numpy comparison

--- questions/9 tyzden ZS/numpy_comparison.py
+++ questions/9 tyzden ZS/numpy_comparison.py
@@ -15,4 +15,14 @@ if __name__ == "__main__":
     print(np.all(array2d[1, :] == 0))
     print(np.all(array2d[1, :] == [0, 0, 0]), end="\n\n")
 
-# https://stackoverflow.com/questions/10580676/comparing-two-numpy-arrays-for-equality-element-wise
+    # How numpy comparison works 
+
+    array_class = type(array2d)
+    print(f"The class is: {array_class}")
+
+    # Show the __eq__ method that *belongs* to this class
+    print(f"The __eq__ method is: {array_class.__eq__}")
+
+    # https://github.com/numpy/numpy/blob/main/numpy/_core/src/multiarray/arrayobject.c (line 823)
+    # https://numpy.org/doc/2.3/reference/generated/numpy.equal.html
+    # https://numpy.org/doc/stable/user/basics.broadcasting.html
